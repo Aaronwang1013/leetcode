cd55554be101e438c83c7dbde98dda6be741e9af
aaron <qazxsw523625@gmail.com>
2022-07-12 00:34:54 +0800

    twosum

---
+++ 01twosum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hash = {}
+        for i , num in enumerate(nums):
+            if target - num in hash:
+                return([hash[target-num] , i])
+                break
+            hash[num] = i
+        return([])
